36b72f8c42cd06cc1fc3fdc393877692b5fadbca
Bitrus John <bitrus.john.bwala13@gmail.com>
2026-08-09 15:34:17 +0100

    Add Screen 2 LoginScreen component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
-import { SignUpScreen } from './Features/auth/SignUpScreen';
+import { LoginScreen } from './Features/auth/LoginScreen';
 
 export default function App() {
-  return <SignUpScreen />;
+  return (
+    <LoginScreen
+      onNavigateToSignUp={() => console.log('Navigate to Sign Up')}
+      onNavigateToForgotPassword={() => console.log('Navigate to Forgot Password')}
+      onLoginSuccess={() => console.log('Login successful!')}
+    />
+  );
 }
